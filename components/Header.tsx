6714876d169915673928e7331a16cc993d099036
KayodeOusi <kayodeolusi@gmail.com>
2022-07-01 18:32:20 +0100

    deploying soon

--- components/Header.tsx
+++ components/Header.tsx
@@ -24,7 +24,7 @@ const Header: FC = () => {
   return (
     <header className="flex bg-white px-4 py-2 shadow-sm sticky top-0 items-center z-50">
       <Link href="/">
-        <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer">
+        <div className="relative h-10 w-20 flex-shrink-0 cursor-pointer mr-0 sm:mr-3">
           <Image
             objectFit="contain"
             src="https://links.papareact.com/fqy"
@@ -33,28 +33,27 @@ const Header: FC = () => {
         </div>
       </Link>
 
-      <div className="flex items-center mx-7 xl:min-w-[300px]">
+      <div className="hidden items-center mx-7 xl:min-w-[300px] lg:flex">
         <HomeIcon className="h-5 w-5" />
         <p className="flex-1 ml-2 hidden lg:inline">Home</p>
-        <ChevronDownIcon className="h-5 w-5" />
       </div>
 
       {/**Search */}
       <form
         className="hidden flex-1 items-center space-x-2 border border-gray-200
-        rounded-sm bg-gray-100 p-3 py-1 lg:flex"
+        rounded-sm bg-gray-100 p-3 py-1 md:flex"
       >
         <SearchIcon className="h-6 w-6 text-gray-400" />
         <input
           className="flex-1 bg-transparent outline-none"
           type="text"
-          placeholder="Search Button"
+          placeholder="Search Reddit"
         />
         <button type="submit" hidden />
       </form>
 
       {/**Icons */}
-      <div className="mx-5 items-center hidden space-x-2 text-gray-500 lg:inline-flex">
+      <div className="mx-5 items-center hidden space-x-2 text-gray-500 sm:inline-flex sm:space-x-6">
         <SparklesIcon className="icon" />
         <GlobeIcon className="icon" />
         <VideoCameraIcon className="icon" />
@@ -64,16 +63,13 @@ const Header: FC = () => {
         <PlusIcon className="icon" />
         <SpeakerphoneIcon className="icon" />
       </div>
-      <div className="ml-5 flex items-center lg:hidden">
-        <MenuIcon className="icon" />
-      </div>
 
       {/**Sign In and Out */}
       {session ? (
         <div
           onClick={() => signOut()}
-          className="hidden items-center lg:flex space-x-2
-          border border-gray-100 p-2 cursor-pointer"
+          className="items-center flex space-x-2 ml-12
+          border border-gray-100 p-2 cursor-pointer sm:ml-0"
         >
           <div className="relative h-5 w-5 flex-shrink-0">
             <Image
@@ -85,7 +81,7 @@ const Header: FC = () => {
           </div>
           <div className="flex-1 text-xs">
             <p className="truncate">{session?.user?.name}</p>
-            <p className="text-gray-400">1 Karma</p>
+            <p className="text-gray-400">Sign Out</p>
           </div>
 
           <ChevronDownIcon className="h-5 flex-shrink-0 text-gray-400" />
@@ -93,8 +89,8 @@ const Header: FC = () => {
       ) : (
         <div
           onClick={() => signIn()}
-          className="hidden items-center lg:flex space-x-2 
-          border border-gray-100 p-2 cursor-pointer"
+          className="items-center flex space-x-2 ml-12 
+          border border-gray-100 p-2 cursor-pointer sm:ml-0"
         >
           <div className="relative h-5 w-5 flex-shrink-0">
             <Image
--- pages/subreddit/[topic].tsx
+++ pages/subreddit/[topic].tsx
@@ -1,4 +1,5 @@
 import { useRouter } from "next/router";
+import { Fragment } from "react";
 import Avatar from "../../components/Avatar";
 import Feed from "../../components/Feed";
 import PostBox from "../../components/PostBox";
@@ -9,26 +10,27 @@ const Subreddit = () => {
   } = useRouter();
 
   return (
-    <div className={`h-24 bg-red-400 p-8`}>
-      <div className="-mx-8 mt-10 bg-white">
-        <div className="mx-auto flex max-w-5xl items-center space-x-4 pb-3">
-          <div className="-mt-10 md:-mt-5">
-            <Avatar seed={topic as string} large />
-          </div>
-          <div className="py-2">
-            <h1 className="text-lg md:text-3xl font-semibold">
-              Welcome to the r/{topic} subreddit
-            </h1>
-            <p className="text-sm">r/{topic}</p>
+    <Fragment>
+      <div className={`h-24 bg-red-400 p-8`}>
+        <div className="-mx-8 mt-10 bg-white">
+          <div className="mx-auto flex max-w-5xl items-center space-x-4 pb-3">
+            <div className="-mt-10 md:-mt-5">
+              <Avatar seed={topic as string} large />
+            </div>
+            <div className="py-2">
+              <h1 className="text-lg md:text-3xl font-semibold">
+                Welcome to the r/{topic} subreddit
+              </h1>
+              <p className="text-sm">r/{topic}</p>
+            </div>
           </div>
         </div>
       </div>
-
-      <div className="mx-auto mt-5 max-w-5xl pb-10">
+      <div className="mx-auto mt-20 max-w-5xl pb-10">
         <PostBox subreddit={topic as string} />
         <Feed topic={topic as string} />
       </div>
-    </div>
+    </Fragment>
   );
 };
 
